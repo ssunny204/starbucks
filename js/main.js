fbf5243ccb1cfd28ecb94d263bdb85b28c407949
ssunny204 <ssunny204@naver.com>
2021-07-12 19:56:36 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function(){
-
-  searchInputEl.focus();
-
-});
-
-searchInputEl.addEventListener("focus", function(){
-
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-
-});
-
-searchInputEl.addEventListener("blur", function(){
-
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-
-});
-
 
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector('#to-top');
@@ -177,10 +154,6 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisyear = document.querySelector('.this-year');
-thisyear.textContent = new Date().getFullYear();
 
 
 
